34386c443fa2bcf5b7353c5c89f1bdbcfae6e05c
Lina Nurullaili_K3521039 <89761692+Linanurulaili@users.noreply.github.com>
2021-10-31 00:19:18 +0700

    Add files via upload

---
+++ chapter 6/ch06.6.py
@@ -0,0 +1,12 @@
+def luasSegitiga1(a,t):
+    luas= a*t/2
+    return luas
+alas= 10
+tinggi= 20
+print('Luas segitiga dengan alas', alas, 'dan tinggi', tinggi, 'adalah', luasSegitiga1(alas,tinggi))
+def luasSegitiga2(a,t):
+    luas= a*t/2
+    return luas
+alas= 15
+tinggi= 45
+print('Luas segitiga dengan alas', alas, 'dan tinggi', tinggi, 'adalah', luasSegitiga2(alas,tinggi))
---
+++ chapter 6/ch06.7.py
@@ -0,0 +1,14 @@
+def luasSegitiga1(a,t):
+    luas= a*t/2
+    print('Luas segitiga dengan alas',alas, 'dan tinggi',tinggi, 'adalah', luas)
+alas=10
+tinggi=20
+luasSegitiga1(alas,tinggi)
+def luasSegitiga2(a,t):
+    luas= a*t/2
+    print('Luas segitiga dengan alas',alas, 'dan tinggi',tinggi
+          , 'adalah', luas)
+alas=15
+tinggi=45
+luasSegitiga2(alas,tinggi)
+
